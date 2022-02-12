0ebab43467dd94fdc0aa5e153484d137389490b1
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-02-11 22:34:16 -0500

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//     res.send('Hello world!')
-// })
-
-// app.listen(3000)
-
-// require('dotenv').config()
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//     res.send('Hello world!')
-// })
-
-// app.listen(process.env.PORT)
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
