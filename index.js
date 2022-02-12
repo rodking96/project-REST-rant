644d6323dec4e89ebda4a0bd1a8e7773a614c5bd
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-02-11 22:02:23 -0500

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// })
+
+// app.listen(3000)
+
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// })
+
+// app.listen(process.env.PORT)
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,4 +25,8 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
